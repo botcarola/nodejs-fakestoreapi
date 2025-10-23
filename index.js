8d583b25a9161d54fd9d6564672e2e642e873fb5
botcarola <celerner4@gmail.com>
2025-10-23 17:09:26 -0300

    tp node js, consumo de api fakestore api

---
+++ index.js
@@ -0,0 +1,41 @@
+const argumentos = process.argv.slice(2)
+
+const fetchData = async (method, endpoint, options = null) => {
+    let response = null
+
+    if (method == "GET" || method == "DELETE") {
+        response = await fetch(`https://fakestoreapi.com/${endpoint}`)
+    } else {
+        response = await fetch(`https://fakestoreapi.com/${endpoint}`, options)
+    }
+
+    const data = await response.json()
+    return data
+}
+
+const showMessage = (method,endpoint, response) => console.log(`Response ${method} ${endpoint}`, response)
+
+const sendRequest = async (method, endpoint, dataBody) => {
+    let response = null
+
+    if (dataBody?.length > 0) {
+
+        const options = {
+            method: method,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                title: dataBody[0],
+                price: dataBody[1],
+                category: dataBody[2],
+            })
+        }
+
+        response = await fetchData(method, endpoint, options)
+        showMessage(method, endpoint, response)
+    } else {
+        response = await fetchData(method, endpoint)
+        showMessage(method, endpoint, response)
+    }    
+}
+
+sendRequest(argumentos[0], argumentos[1], argumentos.slice(2))
